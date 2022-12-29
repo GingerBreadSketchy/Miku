15800c14a5483ffb54c706c52957637b24557488
GingerBreadSketchy <58087375+GingerBreadSketchy@users.noreply.github.com>
2022-12-29 12:55:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ global.prefa = ['.'] // Prefix ---  change it to anythong you want except '@' //
 global.location = "Africa, Nairobi"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
-global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
+global.vidmenu = { url: 'https://media.giphy.com/media/4QxQgWZHbeYwM/giphy.gif' }
 global.websitex = "https://github.com/GingerBreadSketchy"
 global.lolhuman = "KaysaS"
 
@@ -59,18 +59,18 @@ global.ntilink = []
 
 global.mess = {
     jobdone: 'Job done...',
-    useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
+    useradmin: 'Sorry, only *Group Admins* can use this command *Msee!!*!',
     botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
-    botowner: 'Only my *Owner* can use this command, Baka!',
-    grouponly: 'This command is only made for *Groups*, Baka!',
-    privateonly: 'This command is only made for *Private Chat*, Baka!',
+    botowner: 'Only my *Owner* can use this command, Msee!!!',
+    grouponly: 'This command is only made for *Groups*, Msee!!!',
+    privateonly: 'This command is only made for *Private Chat*, Msee!!!',
     botonly: 'Only the *Bot itself* can use this command!',
     waiting: 'Chotto Matte...',
-    nolink: 'Please provide me *link*, Baka!',
-    error: 'An error occurd!',
-    banned: 'You are *Banned* fron using commands!',
+    nolink: 'Please provide me *link*, Msee!!!',
+    error: 'An error occured!',
+    banned: 'You are *Banned* from using commands!',
     bangc: 'This Group is *Banned* from using Commands!',
-    nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+    nonsfw: 'Dont be a pervert Msee!!! This is not a NSFW enabled group!'
     
 }
 
